ddcd935f2afdae9bf0992bd1782ca57c7d3fb824
Ross-Morgan <morgan.ross.512@gmail.com>
2022-10-06 02:30:58 +0100

    comment tokens for clarity

--- src/bml/tokens.py
+++ src/bml/tokens.py
@@ -3,33 +3,34 @@ from enum import Enum, auto
 
 
 class TokenType(Enum):
-    KWD_EXISTS = auto()
-    KWD_FOR = auto()
-    KWD_SET = auto()
-    KWD_SUBSET = auto()
-    KWD_SUPERSET = auto()
+    KWD_EXISTS = auto()    # exists
+    KWD_FOR = auto()       # for
+    KWD_SET = auto()       # set
+    KWD_SUBSET = auto()    # subset
+    KWD_SUPERSET = auto()  # superset
 
-    OP_SQRT = auto()
+    OP_SQRT = auto()  # √
 
-    OP_EQ = auto()
-    OP_ADD = auto()
-    OP_SUB = auto()
-    OP_MUL = auto()
-    OP_DIV = auto()
+    OP_EQ = auto()    # =
+    OP_ADD = auto()   # +
+    OP_SUB = auto()   # -
+    OP_MUL = auto()   #`*
+    OP_DIV = auto()   # /
 
     CONST = auto()
     VAR = auto()
 
-    SYM_CUSTOM = auto()
+    SYM_CUSTOM = auto()   # `<text>
 
-    SYM_COMMA = auto()
+    SYM_ARROW = auto()    # =>
+    SYM_COMMA = auto()    # ,
 
-    SYM_LRPAREN = auto()
-    SYM_RRPAREN = auto()
-    SYM_LSPAREN = auto()
-    SYM_RSPAREN = auto()
-    SYM_LCPAREN = auto()
-    SYM_RCPAREN = auto()
+    SYM_LRPAREN = auto()  # (
+    SYM_RRPAREN = auto()  # [
+    SYM_LSPAREN = auto()  # {
+    SYM_RSPAREN = auto()  # )
+    SYM_LCPAREN = auto()  # ]
+    SYM_RCPAREN = auto()  # }
 
     TYPE_REAL = auto()
     TYPE_COMPLEX = auto()
