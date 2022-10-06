cb0b2d1b79ac0a37be9e78f0172d8ebbca76d434
Ross-Morgan <morgan.ross.512@gmail.com>
2022-10-06 18:08:32 +0100

    add relative imports

--- src/bml/logger/custom_logger.py
+++ src/bml/logger/custom_logger.py
@@ -3,7 +3,11 @@ import logging
 
 DEFAULT_FORMAT = "%(asctime)s | %(levelname)s | %(message)s"
 
+
 class AppLogger:
+    __slots__ = ("format_str", "level", "name", "console_logger",
+                 "console_formatter", "logger")
+
     def __init__(self, name: str, format_str: str = DEFAULT_FORMAT,
                  level: int = logging.INFO) -> None:
         self.format_str = format_str
@@ -17,16 +21,16 @@ class AppLogger:
         self.logger = logging.getLogger()
         self.logger.setLevel(self.level)
         self.logger.addHandler(self.console_logger)
-    
+
     def info(self, msg: str, extra=None):
         self.logger.info(msg, extra=extra)
-    
+
     def error(self, msg: str, extra=None):
         self.logger.error(msg, extra=extra)
-    
+
     def debug(self, msg: str, extra=None):
         self.logger.debug(msg, extra=extra)
-    
+
     def warn(self, msg: str, extra=None):
         self.logger.warn(msg, extra=extra)
 
