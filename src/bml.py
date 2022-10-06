853e4e28def2168c5699f7cf681a427bc8296cba
Ross-Morgan <morgan.ross.512@gmail.com>
2022-10-06 20:07:11 +0100

    add Arg class

--- src/bml.py
+++ src/bml.py
@@ -1,1 +1,20 @@
-from argparse import ArgumentParser
+from argparse import ArgumentParser, Namespace
+
+
+class Args:
+    def __init__(self, namespace: Namespace) -> None:
+        self.filepath: str = namespace.filepath
+        self.verbose: bool = namespace.verbose
+
+    def __repr__(self) -> str:
+        return f"Args(filepath: {self.filepath}, verbose: {self.verbose})"
+
+
+arg_parser = ArgumentParser()
+
+arg_parser.add_argument("filepath", required=True, help="file to interpret")
+arg_parser.add_argument("-v", "--verbose", type=bool, default=False)
+
+args = Args(arg_parser.parse_args())
+
+print(args)
