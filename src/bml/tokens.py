805090068ca8d514f49beb45a5fe2995cb36fa12
Ross Morgan <68851224+Ross-Morgan@users.noreply.github.com>
2022-10-06 12:15:17 +0100

    add more set keywords

--- src/bml/tokens.py
+++ src/bml/tokens.py
@@ -3,14 +3,15 @@ from enum import Enum, auto
 
 
 class TokenType(Enum):
-    KWD_EXISTS = auto()    # exists
-    KWD_FOR = auto()       # for
-    KWD_SET = auto()       # set
-    KWD_SUBSET = auto()    # subset
-    KWD_SUPERSET = auto()  # superset
+    KWD_EXISTS = auto()        # exists
+    KWD_FOR = auto()           # for
+    KWD_SET = auto()           # set
+    KWD_SUBSET = auto()        # subset
+    KWD_SUPERSET = auto()      # superset
+    KWD_UNION = auto()         # union
+    KWD_INTERSECTION = auto()  # interection
 
     OP_SQRT = auto()  # √
-
     OP_EQ = auto()    # =
     OP_ADD = auto()   # +
     OP_SUB = auto()   # -
