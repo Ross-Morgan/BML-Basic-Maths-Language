19e3f86ab9125786f56370a80c23208bbd16c410
Ross-Morgan <morgan.ross.512@gmail.com>
2022-10-09 00:46:16 +0100

    move syntax list to module

---
+++ src/bml/syntax/chars.py
@@ -0,0 +1,26 @@
+__all__ = [
+    "BACKTICK", "SQRT", "U_PHI", "L_PHI", "PI"
+]
+
+KEYWORDS = [
+    "set", "exists", "for", "superset", "subset", "difference"
+]
+
+
+REAL_DIGITS = "0123456789."
+COMPLEX_DIGITS = REAL_DIGITS + "i"
+
+OPERATORS = "+-/*^√=<>≤≥"
+
+COMMA = ","
+BACKTICK = "`"
+EQUALS = "="
+
+PARENS = r"([{}])"
+
+SQRT = "√"
+
+U_PHI = "ϕ"
+L_PHI = "φ"
+
+PI = "π"
