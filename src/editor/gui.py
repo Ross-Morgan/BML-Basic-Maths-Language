52629c19d50c78a72f2fbf0cd64875848062af7f
Ross Morgan <morgan.ross.512@gmail.com>
2022-10-09 22:46:42 +0100

    connect highlighting

--- src/editor/gui.py
+++ src/editor/gui.py
@@ -1,5 +1,3 @@
-import sys
-
 from PyQt6 import QtCore, QtGui, QtWidgets
 
 from . import highlighter
@@ -16,15 +14,17 @@ class EditorWidget(QtWidgets.QWidget):
         self.setLayout(self.layout)
 
         self.setup_ui()
+        self.setup_editor()
 
     def setup_ui(self):
         self.text_box = QtWidgets.QPlainTextEdit(self)
         self.text_box.resize(self.size())
         self.text_box.setFont(QtGui.QFont("Consolas"))
-        self.text_box.setStyleSheet("margin-top: 20px")
+        self.text_box.setStyleSheet("margin-top: 20px; background-color: #232323; color: white;")  # noqa
 
     def setup_editor(self):
-        self.highlighter = highlighter.new_highlighter(self.text_box.document())
+        self.highlighter = highlighter.new_highlighter()
+        self.highlighter.setDocument(self.text_box.document())
 
     def resizeEvent(self, new: QtGui.QResizeEvent) -> None:
         self.text_box.resize(new.size())
