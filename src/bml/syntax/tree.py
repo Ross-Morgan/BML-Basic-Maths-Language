1a42eaffb568e238bca2a68649f55ea4165b25b2
Ross Morgan <morgan.ross.512@gmail.com>
2022-10-13 23:05:00 +0100

    add base node

--- src/bml/syntax/tree.py
+++ src/bml/syntax/tree.py
@@ -1,3 +1,13 @@
+from dataclasses import dataclass
+from . import Node
+
+
 class Tree:
     def __init__(self) -> None:
         self.nodes = []
+
+
+@dataclass(slots=True)
+class BaseNode(Node):
+    lhs: Node
+    rhs: Node
