41f07d1415dc74ac5e7c101470349862c97788f2
Ross Morgan <morgan.ross.512@gmail.com>
2022-10-13 23:04:53 +0100

    add base node

--- src/bml/syntax/__init__.py
+++ src/bml/syntax/__init__.py
@@ -2,5 +2,24 @@
 # BML Syntax Tree
 """
 
-from . import nodes
-from . import tree
+from __future__ import annotations
+
+from typing import TypeVar
+
+from . import nodes, tree
+
+N = TypeVar("N")
+L = TypeVar("L")
+R = TypeVar("R")
+
+
+class Node:
+    """Base class for syntax tree nodes"""
+
+    def __init__(self, lhs: L | N, rhs: R | N):
+        self.lhs = lhs
+        self.rhs = rhs
+
+    def compute(self): ...
+
+
--- src/bml/syntax/nodes.py
+++ src/bml/syntax/nodes.py
@@ -1,6 +1,26 @@
 from dataclasses import dataclass
+from typing import Callable, TypeVar
+
+from . import Node
+
+N = TypeVar("N")
+N_co = TypeVar("N_co", covariant=True)
 
 
-@dataclass(frozen=True, slots=True)
-class ModNode:
-    lhs: int | float | complex
+@dataclass(slots=True)
+class SymbolNode(Node):
+    symbol_name: str
+
+
+@dataclass(slots=True)
+class NumericNode(Node):
+    operator: Callable[[N, N], N_co]
+
+    lhs: int | float | complex | Node
+    rhs: int | float | complex | Node
+
+
+@dataclass(slots=True)
+class AssignmentNode(Node):
+    symbol: str
+    value: object
