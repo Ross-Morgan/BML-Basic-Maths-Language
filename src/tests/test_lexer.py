6a3d46828a07887a4c9f8f3a47651e6834121509
Ross-Morgan <morgan.ross.512@gmail.com>
2022-10-18 22:56:11 +0100

    add lexer tests

---
+++ src/tests/test_lexer.py
@@ -0,0 +1,18 @@
+import textwrap
+
+import bml
+
+from .tests import test
+
+
+@test
+def test_symbol_parsing():
+    source = textwrap.dedent("""\
+    `symbol = 20
+    `symbol_2 = `symbol * 2
+    """)
+
+    print(source)
+
+
+test_symbol_parsing()
