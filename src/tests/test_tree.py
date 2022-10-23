57ab20409c496334d46843a86c0f0e33ad16da42
Ross-Morgan <morgan.ross.512@gmail.com>
2022-10-23 14:33:10 +0100

    add tree compilation test

--- src/tests/test_tree.py
+++ src/tests/test_tree.py
@@ -1,11 +1,33 @@
-import bml
-from .tests import test
+from bml.syntax import MultiNodeTree, SingleNodeTree, nodes
+
+import load_path  # noqa NOSONAR
 
 
-@test
-def test_tree():
-    tree = bml.syntax.Tree()
-    tree.add_node(bml.syntax.nodes.AssignmentNode("a", 100))
-    tree.add_node(bml.syntax.nodes.AssignmentNode("b", bml.syntax.nodes.SymbolNode("a")))
+def test_tree_compilation():
+    tree = MultiNodeTree()
 
-    print(tree)
+    a = nodes.AssignmentNode("a", 100)
+    b = nodes.AssignmentNode("b", nodes.SymbolNode("a"))
+    c = nodes.AssignmentNode("c", nodes.SymbolNode("b"))
+    d = nodes.AssignmentNode("d", nodes.SymbolNode("c"))
+    e = nodes.AssignmentNode("e", nodes.SymbolNode("d"))
+    f = nodes.AssignmentNode("f", nodes.SymbolNode("e"))
+
+    tree.add_node(a)
+    tree.add_node(b)
+    tree.add_node(c)
+    tree.add_node(d)
+    tree.add_node(e)
+    tree.add_node(f)
+
+    new_tree = tree.compile()
+
+    assert isinstance(new_tree, SingleNodeTree)
+
+    assert new_tree.lhs is a
+    assert new_tree.rhs.lhs is b
+    assert new_tree.rhs.rhs.lhs is c
+    assert new_tree.rhs.rhs.rhs.lhs is d
+    assert new_tree.rhs.rhs.rhs.rhs.lhs is e
+    assert new_tree.rhs.rhs.rhs.rhs.rhs.lhs is f
+
